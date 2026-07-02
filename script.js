20231ebfa96c22fb02e39e3fd90d0d6a695308cc
VexTester <coocksucker99@gmail.com>
2026-07-02 16:56:06 +0200

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const music = document.getElementById("bg-music");
 
-document.addEventListener("click", () => {
+window.addEventListener("click", () => {
     music.volume = 0.35;
     music.play().catch(() => {});
 }, { once: true });
