0cfeaa1bfe4733b969dbafa0f3f30820acedbca3
VexTester <coocksucker99@gmail.com>
2026-07-02 17:23:15 +0200

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ window.addEventListener("click", () => {
     music.volume = 0.35;
     music.play().catch(() => {});
 }, { once: true });
+
+document.addEventListener("visibilitychange", () => {
+    const music = document.getElementById("bg-music");
+
+    if (document.hidden) {
+        music.pause();
+    } else {
+        music.play().catch(() => {});
+    }
+});
